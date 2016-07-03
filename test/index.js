7bd6b8716d32ca6f2936988778e6811fed5c78d3
David Sancho <dsnxmoreno@gmail.com>
2016-07-03 20:37:17 +0200

    Refactor(test): Use tape instead of mocha

--- test/index.js
+++ test/index.js
@@ -1,25 +1,2 @@
-import expect from 'expect'
-import reducers from './../src/reducers'
-import { ACTIONS as ACTION } from './../src/constants'
-
-describe('#reducers', () => {
-  describe('openPanelView', () => {
-    it('should be opening panel visibile on the global state', () => {
-      const expectedState = {
-        isPanelVisible: true
-      }
-      const nextState = reducers(null, { type: ACTION.OPEN_PANEL_VIEW })
-      expect(expectedState).toEqual(nextState)
-    })
-  })
-
-  describe('closePanelView', () => {
-    it('should be closing panel visibile on the global state', () => {
-      const expectedState = {
-        isPanelVisible: false
-      }
-      const nextState = reducers(null, { type: ACTION.CLOSE_PANEL_VIEW })
-      expect(expectedState).toEqual(nextState)
-    })
-  })
-})
+import requireDir from 'require-dir'
+requireDir('./')
