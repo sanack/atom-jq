b7fee48b3ae75792db9e84b3e7c73476ec23faca
David Sancho <dsnxmoreno@gmail.com>
2016-07-03 20:11:14 +0200

    implement(test): Testing reducers

--- test/index.js
+++ test/index.js
@@ -1,5 +1,25 @@
 import expect from 'expect'
+import reducers from './../src/reducers'
+import { ACTIONS as ACTION } from './../src/constants'
 
-describe('Dummy test', function () {
-  expect(false).toBeTruthy()
+describe('#reducers', () => {
+  describe('openPanelView', () => {
+    it('should be opening panel visibile on the global state', () => {
+      const expectedState = {
+        isPanelVisible: true
+      }
+      const nextState = reducers(null, { type: ACTION.OPEN_PANEL_VIEW })
+      expect(expectedState).toEqual(nextState)
+    })
+  })
+
+  describe('closePanelView', () => {
+    it('should be closing panel visibile on the global state', () => {
+      const expectedState = {
+        isPanelVisible: false
+      }
+      const nextState = reducers(null, { type: ACTION.CLOSE_PANEL_VIEW })
+      expect(expectedState).toEqual(nextState)
+    })
+  })
 })
