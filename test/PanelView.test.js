74fd0d03d461d8bc0388553984a4ad743e8d30bf
David Sancho <dsnxmoreno@gmail.com>
2016-07-04 18:33:57 +0200

    test(PanelView): Just static test

---
+++ test/PanelView.test.js
@@ -0,0 +1,32 @@
+/** @babel */
+
+import test from 'tape'
+import PanelView from './../src/PanelView'
+import React from 'react'
+import { render } from 'enzyme'
+import reducers from './../src/reducers'
+import { createStore } from 'redux'
+
+test('<PanelView />', (assert) => {
+  const store = createStore(reducers)
+  const mockStaticRender = render(<PanelView store={store} />)
+  assert.equal(
+    mockStaticRender.find('.jq-panel').length,
+    1,
+    'contains jq-panel css class'
+  )
+
+  assert.equal(
+    mockStaticRender.find('input').length,
+    1,
+    'contains a input'
+  )
+
+  assert.equal(
+    mockStaticRender.find('button').length,
+    1,
+    'contains a button'
+  )
+
+  assert.end()
+})
