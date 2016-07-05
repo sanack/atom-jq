2422f0778243a87ad7ee5edd430c768c543caca9
David Sancho <david.sancho@typeform.com>
2016-07-05 09:07:16 +0200

    Tweak styles (#17)
    * Rename AtomTextInput and finish styles
    
    * Add sagas for run async jq
    
    * Implement try catch on saga jq
    
    * Remove styles not used
    
    * Remove commented thunk code in saga
    
    * Fix sagas and add call as a side effect
    
    * Add openModalSaga with the atom.newpane
    
    * Keep openModalListener listening all the time
    
    * Remove all the stuff that is not necesary
    
    * Add focus on input after render
    
    * Refactor, open close panelView
    
    * Abstract workspace utils outside from sagas
    
    * Fixed package.json
    
    * Add collapsing to the logger
    
    * Make it work again
    
    * implement(test): First dummy test
    
    * Fix(PanelView): Appears as a workspace bottomPanel
    
    * Implement(styles): PanelView with input inheriting some styles from the theme

--- test/index.js
+++ test/index.js
@@ -1,10 +1,16 @@
 <<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> dee1f7c3f894541944318611be5c38c1061ce4ad
 import expect from 'expect'
 
 describe('Dummy test', function () {
   expect(false).toBeTruthy()
 })
+<<<<<<< HEAD
+=======
 =======
 import requireDir from 'require-dir'
 requireDir('./')
 >>>>>>> 5fb102940d94a6d52f12cc979377c4b349fd3b6b
+>>>>>>> dee1f7c3f894541944318611be5c38c1061ce4ad
