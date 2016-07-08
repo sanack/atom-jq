d5e55241a66d37f45b0404be3d4e1d0f85715573
David Sancho <dsnxmoreno@gmail.com>
2016-07-08 11:28:14 +0200

    Improve(debug): Use loggin on debug mode

--- src/store.js
+++ src/store.js
@@ -9,7 +9,7 @@ import { isAtomInDebugMode } from './debug'
 
 const saga = createSagaMiddleware()
 let middlewares = [saga]
-if (isAtomInDebugMode) {
+if (isAtomInDebugMode()) {
   const logger = createLogger({ collapsed: true })
   middlewares.push(logger)
 }
