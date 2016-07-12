ea5cdd27edcb73b19da9da1468eeff99ac2d01b6
David Sancho <dsnxmoreno@gmail.com>
2016-07-12 22:24:49 +0200

    Delete(tests): Unnecesary test on jasmine

--- spec/index-spec.js
+++ spec/index-spec.js
@@ -1,33 +1,14 @@
-// import test from 'tape'
-// import atomJq from './../src'
-//
-// test('atom-jq export', (assert) => {
-//   assert.equal(
-//     atomJq.hasOwnProperty('activate'),
-//     true,
-//     'should have a activate property'
-//   )
-//
-//   assert.equal(
-//     atomJq.hasOwnProperty('deactivate'),
-//     true,
-//     'should have a deactivate property'
-//   )
-//
-//   assert.end()
-// })
+import atomJq from './../lib'
 
-// const atomJq = require('../../lib')
-//
-// describe('#atom-jq export', () => {
-//   it('should have a activate property', () => {
-//     expect(atomJq.hasOwnProperty('activate')).toBe(true)
-//   })
-//
-//   it('should have a activate property', () => {
-//     expect(atomJq.hasOwnProperty('activate')).toBe(true)
-//   })
-// })
+describe('#atom-jq export', () => {
+  it('should have a activate property', () => {
+    expect(atomJq.hasOwnProperty('activate')).toBe(true)
+  })
+
+  it('should have a activate property', () => {
+    expect(atomJq.hasOwnProperty('activate')).toBe(true)
+  })
+})
 
 describe('#atom-jq', () => {
   it('', () => {
