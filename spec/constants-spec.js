0e9178f35efc25814d76d7947c85019e1323b0dd
David Sancho <dsnxmoreno@gmail.com>
2016-07-24 16:21:10 +0200

    Fix(ci): Compile spec on travis

--- spec/constants-spec.js
+++
@@ -1,5 +0,0 @@
-export const ROOT_DOM_CLASS = '.atom-jq-root'
-export const PACKAGE_NAME = 'atom-jq'
-export const OPEN_COMMAND = ':open'
-export const CLOSE_COMMAND = ':close'
-export const JQ_PANEL_HIDDEN_CLASS = 'jq-panel__hidden'
