e3339739898379b1139ebe32d4668e64a68a52eb
David Sancho <dsnxmoreno@gmail.com>
2016-07-24 13:14:49 +0200

    Add(spec): Write the scenarios to test the interaction

---
+++ spec/interaction-spec.js
@@ -0,0 +1,22 @@
+/** @babel */
+
+
+describe('Run a jq query', () => {
+  describe('with a valid JSON file', () => {
+    it('should open a new tab with the result', () => {
+
+    })
+  })
+
+  describe('with a invalid JSON file', () => {
+    it('should open a new error', () => {
+
+    })
+  })
+
+  describe('with a invalid query', () => {
+    it('should open a new error', () => {
+
+    })
+  })
+})
